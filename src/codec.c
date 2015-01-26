96e43239bfe96c9af91e10c8d02fbca9bc0637a2
walker <18622283215@163.com>
2015-01-26 15:45:27 +0800

    把tab替换为两个空格

--- src/codec.c
+++ src/codec.c
@@ -17,23 +17,23 @@
  */
 static int codec_base64_encode(lua_State *L)
 {
-	size_t len;
-	const char *bs = luaL_checklstring(L, 1, &len);
-	BIO *b64 = BIO_new(BIO_f_base64());
-	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-	BIO *bio = BIO_new(BIO_s_mem());
-	bio = BIO_push(b64, bio);
-	BIO_write(bio, bs, len);
-	BIO_flush(bio);
-	BUF_MEM *p;
-	BIO_get_mem_ptr(bio, &p);
-	int n = p->length;
-	char dst[n];
-	memcpy(dst, p->data, n);
-	BIO_free_all(bio);
+  size_t len;
+  const char *bs = luaL_checklstring(L, 1, &len);
+  BIO *b64 = BIO_new(BIO_f_base64());
+  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+  BIO *bio = BIO_new(BIO_s_mem());
+  bio = BIO_push(b64, bio);
+  BIO_write(bio, bs, len);
+  BIO_flush(bio);
+  BUF_MEM *p;
+  BIO_get_mem_ptr(bio, &p);
+  int n = p->length;
+  char dst[n];
+  memcpy(dst, p->data, n);
+  BIO_free_all(bio);
 
-	lua_pushlstring(L, dst, n);
-	return 1;
+  lua_pushlstring(L, dst, n);
+  return 1;
 }
 
 /**
@@ -46,18 +46,18 @@ static int codec_base64_encode(lua_State *L)
  */
 static int codec_base64_decode(lua_State *L)
 {
-	size_t len;
-	const char *cs = luaL_checklstring(L, 1, &len);
-	BIO *b64 = BIO_new(BIO_f_base64());
-	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-	BIO *bio = BIO_new_mem_buf((void *)cs, len);
-	bio = BIO_push(b64, bio);
-	char dst[len];
-	int n = BIO_read(bio, dst, len);
-	BIO_free_all(bio);
+  size_t len;
+  const char *cs = luaL_checklstring(L, 1, &len);
+  BIO *b64 = BIO_new(BIO_f_base64());
+  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+  BIO *bio = BIO_new_mem_buf((void *)cs, len);
+  bio = BIO_push(b64, bio);
+  char dst[len];
+  int n = BIO_read(bio, dst, len);
+  BIO_free_all(bio);
 
-	lua_pushlstring(L, dst, n);
-	return 1;
+  lua_pushlstring(L, dst, n);
+  return 1;
 }
 
 /**
@@ -72,43 +72,43 @@ static int codec_base64_decode(lua_State *L)
  */
 static int codec_aes_encrypt(lua_State *L)
 {
-	size_t len;
-	const char *src = luaL_checklstring(L, 1, &len);
-	char *key = luaL_checkstring(L, 2);
+  size_t len;
+  const char *src = luaL_checklstring(L, 1, &len);
+  char *key = luaL_checkstring(L, 2);
 
-	EVP_CIPHER_CTX ctx;
-	EVP_CIPHER_CTX_init(&ctx);
+  EVP_CIPHER_CTX ctx;
+  EVP_CIPHER_CTX_init(&ctx);
 
-	int ret = EVP_EncryptInit_ex(&ctx, EVP_aes_128_ecb(), NULL, (unsigned char *)key, NULL);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP encrypt init error");
-	}
+  int ret = EVP_EncryptInit_ex(&ctx, EVP_aes_128_ecb(), NULL, (unsigned char *)key, NULL);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP encrypt init error");
+  }
 
-	int dstn = len + 128, n, wn;
-	char dst[dstn];
-	memset(dst, 0, dstn);
+  int dstn = len + 128, n, wn;
+  char dst[dstn];
+  memset(dst, 0, dstn);
 
-	ret = EVP_EncryptUpdate(&ctx, (unsigned char *)dst, &wn, (unsigned char *)src, len);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP encrypt update error");
-	}
-	n = wn;
+  ret = EVP_EncryptUpdate(&ctx, (unsigned char *)dst, &wn, (unsigned char *)src, len);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP encrypt update error");
+  }
+  n = wn;
 
-	ret = EVP_EncryptFinal_ex(&ctx, (unsigned char *)(dst + n), &wn);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP encrypt final error");
-	}
-	EVP_CIPHER_CTX_cleanup(&ctx);
-	n += wn;
+  ret = EVP_EncryptFinal_ex(&ctx, (unsigned char *)(dst + n), &wn);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP encrypt final error");
+  }
+  EVP_CIPHER_CTX_cleanup(&ctx);
+  n += wn;
 
-	lua_pushlstring(L, dst, n);
-	return 1;
+  lua_pushlstring(L, dst, n);
+  return 1;
 }
 
 /**
@@ -123,43 +123,43 @@ static int codec_aes_encrypt(lua_State *L)
  */
 static int codec_aes_decrypt(lua_State *L)
 {
-	size_t len;
-	const char *src = luaL_checklstring(L, 1, &len);
-	char *key = luaL_checkstring(L, 2);
+  size_t len;
+  const char *src = luaL_checklstring(L, 1, &len);
+  char *key = luaL_checkstring(L, 2);
 
-	EVP_CIPHER_CTX ctx;
-	EVP_CIPHER_CTX_init(&ctx);
+  EVP_CIPHER_CTX ctx;
+  EVP_CIPHER_CTX_init(&ctx);
 
-	int ret = EVP_DecryptInit_ex(&ctx, EVP_aes_128_ecb(), NULL, (unsigned char *)key, NULL);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP decrypt init error");
-	}
+  int ret = EVP_DecryptInit_ex(&ctx, EVP_aes_128_ecb(), NULL, (unsigned char *)key, NULL);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP decrypt init error");
+  }
 
-	int n, wn;
-	char dst[len];
-	memset(dst, 0, len);
+  int n, wn;
+  char dst[len];
+  memset(dst, 0, len);
 
-	ret = EVP_DecryptUpdate(&ctx, (unsigned char *)dst, &wn, (unsigned char *)src, len);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP decrypt update error");
-	}
-	n = wn;
+  ret = EVP_DecryptUpdate(&ctx, (unsigned char *)dst, &wn, (unsigned char *)src, len);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP decrypt update error");
+  }
+  n = wn;
 
-	ret = EVP_DecryptFinal_ex(&ctx, (unsigned char *)(dst + n), &wn);
-	if(ret != 1)
-	{
-		EVP_CIPHER_CTX_cleanup(&ctx);
-		return luaL_error(L, "EVP decrypt final error");
-	}
-	EVP_CIPHER_CTX_cleanup(&ctx);
-	n += wn;
+  ret = EVP_DecryptFinal_ex(&ctx, (unsigned char *)(dst + n), &wn);
+  if(ret != 1)
+  {
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return luaL_error(L, "EVP decrypt final error");
+  }
+  EVP_CIPHER_CTX_cleanup(&ctx);
+  n += wn;
 
-	lua_pushlstring(L, dst, n);
-	return 1;
+  lua_pushlstring(L, dst, n);
+  return 1;
 }
 
 /**
@@ -174,60 +174,60 @@ static int codec_aes_decrypt(lua_State *L)
  */
 static int codec_rsa_private_sign(lua_State *L)
 {
-	size_t len;
-	const char *src = luaL_checklstring(L, 1, &len);
-	char *pem = luaL_checkstring(L, 2);
+  size_t len;
+  const char *src = luaL_checklstring(L, 1, &len);
+  char *pem = luaL_checkstring(L, 2);
 
-	SHA_CTX c;
-	unsigned char sha[SHA_DIGEST_LENGTH];
-	memset(sha, 0, SHA_DIGEST_LENGTH);
-	if(SHA_Init(&c) != 1)
-	{
-		OPENSSL_cleanse(&c, sizeof(c));
-		return luaL_error(L, "SHA init error");
-	}
-	if(SHA1_Update(&c, src, len) != 1)
-	{
-		OPENSSL_cleanse(&c, sizeof(c));
-		return luaL_error(L, "SHA update error");
-	}
-	if(SHA1_Final(sha, &c) != 1)
-	{
-		OPENSSL_cleanse(&c, sizeof(c));
-		return luaL_error(L, "SHA update error");
-	}
-	OPENSSL_cleanse(&c, sizeof(c));
+  SHA_CTX c;
+  unsigned char sha[SHA_DIGEST_LENGTH];
+  memset(sha, 0, SHA_DIGEST_LENGTH);
+  if(SHA_Init(&c) != 1)
+  {
+    OPENSSL_cleanse(&c, sizeof(c));
+    return luaL_error(L, "SHA init error");
+  }
+  if(SHA1_Update(&c, src, len) != 1)
+  {
+    OPENSSL_cleanse(&c, sizeof(c));
+    return luaL_error(L, "SHA update error");
+  }
+  if(SHA1_Final(sha, &c) != 1)
+  {
+    OPENSSL_cleanse(&c, sizeof(c));
+    return luaL_error(L, "SHA update error");
+  }
+  OPENSSL_cleanse(&c, sizeof(c));
 
-	BIO *bio = BIO_new_mem_buf((void *)pem, -1);
-	if(bio == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "PEM error");
-	}
-	RSA *rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
-	if(rsa == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA read private key error");
-	}
-	BIO_free_all(bio);
+  BIO *bio = BIO_new_mem_buf((void *)pem, -1);
+  if(bio == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "PEM error");
+  }
+  RSA *rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
+  if(rsa == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA read private key error");
+  }
+  BIO_free_all(bio);
 
-	int n = RSA_size(rsa), wn;
-	char dst[n];
-	memset(dst, 0, n);
+  int n = RSA_size(rsa), wn;
+  char dst[n];
+  memset(dst, 0, n);
 
-	int ret = RSA_sign(NID_sha1, (unsigned char *)sha, SHA_DIGEST_LENGTH, (unsigned char *)dst, (unsigned int *)&wn,
-	        rsa);
-	if(ret != 1)
-	{
-		RSA_free(rsa);
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA sign error");
-	}
-	RSA_free(rsa);
+  int ret = RSA_sign(NID_sha1, (unsigned char *)sha, SHA_DIGEST_LENGTH, (unsigned char *)dst, (unsigned int *)&wn,
+          rsa);
+  if(ret != 1)
+  {
+    RSA_free(rsa);
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA sign error");
+  }
+  RSA_free(rsa);
 
-	lua_pushlstring(L, dst, wn);
-	return 1;
+  lua_pushlstring(L, dst, wn);
+  return 1;
 }
 
 /**
@@ -243,51 +243,51 @@ static int codec_rsa_private_sign(lua_State *L)
  */
 static int codec_rsa_public_verify(lua_State *L)
 {
-	size_t srclen, signlen;
-	const char *src = luaL_checklstring(L, 1, &srclen);
-	const char *sign = luaL_checklstring(L, 2, &signlen);
-	char *pem = luaL_checkstring(L, 3);
+  size_t srclen, signlen;
+  const char *src = luaL_checklstring(L, 1, &srclen);
+  const char *sign = luaL_checklstring(L, 2, &signlen);
+  char *pem = luaL_checkstring(L, 3);
 
-	SHA_CTX ctx;
-	int ctxlen = sizeof(ctx);
-	unsigned char sha[SHA_DIGEST_LENGTH];
-	memset(sha, 0, SHA_DIGEST_LENGTH);
-	if(SHA_Init(&ctx) != 1)
-	{
-		OPENSSL_cleanse(&ctx, ctxlen);
-		return luaL_error(L, "SHA init error");
-	}
-	if(SHA1_Update(&ctx, src, srclen) != 1)
-	{
-		OPENSSL_cleanse(&ctx, ctxlen);
-		return luaL_error(L, "SHA update error");
-	}
-	if(SHA1_Final(sha, &ctx) != 1)
-	{
-		OPENSSL_cleanse(&ctx, ctxlen);
-		return luaL_error(L, "SHA update error");
-	}
-	OPENSSL_cleanse(&ctx, ctxlen);
+  SHA_CTX ctx;
+  int ctxlen = sizeof(ctx);
+  unsigned char sha[SHA_DIGEST_LENGTH];
+  memset(sha, 0, SHA_DIGEST_LENGTH);
+  if(SHA_Init(&ctx) != 1)
+  {
+    OPENSSL_cleanse(&ctx, ctxlen);
+    return luaL_error(L, "SHA init error");
+  }
+  if(SHA1_Update(&ctx, src, srclen) != 1)
+  {
+    OPENSSL_cleanse(&ctx, ctxlen);
+    return luaL_error(L, "SHA update error");
+  }
+  if(SHA1_Final(sha, &ctx) != 1)
+  {
+    OPENSSL_cleanse(&ctx, ctxlen);
+    return luaL_error(L, "SHA update error");
+  }
+  OPENSSL_cleanse(&ctx, ctxlen);
 
-	BIO *bio = BIO_new_mem_buf((void *)pem, -1);
-	if(bio == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "PEM error");
-	}
-	RSA *rsa = PEM_read_bio_RSA_PUBKEY(bio, NULL, NULL, NULL);
-	if(rsa == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA read public key error");
-	}
-	BIO_free_all(bio);
+  BIO *bio = BIO_new_mem_buf((void *)pem, -1);
+  if(bio == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "PEM error");
+  }
+  RSA *rsa = PEM_read_bio_RSA_PUBKEY(bio, NULL, NULL, NULL);
+  if(rsa == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA read public key error");
+  }
+  BIO_free_all(bio);
 
-	int ret = RSA_verify(NID_sha1, sha, SHA_DIGEST_LENGTH, (unsigned char *)sign, signlen, rsa);
-	RSA_free(rsa);
+  int ret = RSA_verify(NID_sha1, sha, SHA_DIGEST_LENGTH, (unsigned char *)sign, signlen, rsa);
+  RSA_free(rsa);
 
-	lua_pushboolean(L, ret);
-	return 1;
+  lua_pushboolean(L, ret);
+  return 1;
 }
 
 /**
@@ -302,39 +302,39 @@ static int codec_rsa_public_verify(lua_State *L)
  */
 static int codec_rsa_public_encrypt(lua_State *L)
 {
-	size_t len;
-	const char *src = luaL_checklstring(L, 1, &len);
-	char *pem = luaL_checkstring(L, 2);
+  size_t len;
+  const char *src = luaL_checklstring(L, 1, &len);
+  char *pem = luaL_checkstring(L, 2);
 
-	BIO *bio = BIO_new_mem_buf((void *)pem, -1);
-	if(bio == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "PEM error");
-	}
-	RSA *rsa = PEM_read_bio_RSA_PUBKEY(bio, NULL, NULL, NULL);
-	if(rsa == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA read public key error");
-	}
-	BIO_free_all(bio);
+  BIO *bio = BIO_new_mem_buf((void *)pem, -1);
+  if(bio == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "PEM error");
+  }
+  RSA *rsa = PEM_read_bio_RSA_PUBKEY(bio, NULL, NULL, NULL);
+  if(rsa == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA read public key error");
+  }
+  BIO_free_all(bio);
 
-	int n = RSA_size(rsa);
-	char dst[n];
-	memset(dst, 0, n);
+  int n = RSA_size(rsa);
+  char dst[n];
+  memset(dst, 0, n);
 
-	int ret = RSA_public_encrypt(len, (unsigned char *)src, (unsigned char *)dst, rsa, RSA_PKCS1_PADDING);
-	if(ret != n)
-	{
-		RSA_free(rsa);
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA public encrypt error");
-	}
-	RSA_free(rsa);
+  int ret = RSA_public_encrypt(len, (unsigned char *)src, (unsigned char *)dst, rsa, RSA_PKCS1_PADDING);
+  if(ret != n)
+  {
+    RSA_free(rsa);
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA public encrypt error");
+  }
+  RSA_free(rsa);
 
-	lua_pushlstring(L, dst, n);
-	return 1;
+  lua_pushlstring(L, dst, n);
+  return 1;
 }
 
 /**
@@ -349,55 +349,55 @@ static int codec_rsa_public_encrypt(lua_State *L)
  */
 static int codec_rsa_private_decrypt(lua_State *L)
 {
-	const char *src = luaL_checkstring(L, 1);
-	char *pem = luaL_checkstring(L, 2);
+  const char *src = luaL_checkstring(L, 1);
+  char *pem = luaL_checkstring(L, 2);
 
-	BIO *bio = BIO_new_mem_buf((void *)pem, -1);
-	if(bio == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "PEM error");
-	}
-	RSA *rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
-	if(rsa == NULL)
-	{
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA read private key error");
-	}
-	BIO_free_all(bio);
+  BIO *bio = BIO_new_mem_buf((void *)pem, -1);
+  if(bio == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "PEM error");
+  }
+  RSA *rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
+  if(rsa == NULL)
+  {
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA read private key error");
+  }
+  BIO_free_all(bio);
 
-	int n = RSA_size(rsa);
-	char dst[n];
-	memset(dst, 0, n);
+  int n = RSA_size(rsa);
+  char dst[n];
+  memset(dst, 0, n);
 
-	int ret = RSA_private_decrypt(n, (unsigned char *)src, (unsigned char *)dst, rsa, RSA_PKCS1_PADDING);
-	if(ret <= 0)
-	{
-		RSA_free(rsa);
-		BIO_free_all(bio);
-		return luaL_error(L, "RSA private decrypt error");
-	}
-	RSA_free(rsa);
+  int ret = RSA_private_decrypt(n, (unsigned char *)src, (unsigned char *)dst, rsa, RSA_PKCS1_PADDING);
+  if(ret <= 0)
+  {
+    RSA_free(rsa);
+    BIO_free_all(bio);
+    return luaL_error(L, "RSA private decrypt error");
+  }
+  RSA_free(rsa);
 
-	lua_pushlstring(L, dst, ret);
-	return 1;
+  lua_pushlstring(L, dst, ret);
+  return 1;
 }
 
 static const struct luaL_Reg codec[] =
 {
-	{"base64_encode", codec_base64_encode},
-	{"base64_decode", codec_base64_decode},
-	{"aes_encrypt", codec_aes_encrypt},
-	{"aes_decrypt", codec_aes_decrypt},
-	{"rsa_private_sign", codec_rsa_private_sign},
-	{"rsa_public_verify", codec_rsa_public_verify},
-	{"rsa_public_encrypt", codec_rsa_public_encrypt},
-	{"rsa_private_decrypt", codec_rsa_private_decrypt},
-	{NULL, NULL}
+  {"base64_encode", codec_base64_encode},
+  {"base64_decode", codec_base64_decode},
+  {"aes_encrypt", codec_aes_encrypt},
+  {"aes_decrypt", codec_aes_decrypt},
+  {"rsa_private_sign", codec_rsa_private_sign},
+  {"rsa_public_verify", codec_rsa_public_verify},
+  {"rsa_public_encrypt", codec_rsa_public_encrypt},
+  {"rsa_private_decrypt", codec_rsa_private_decrypt},
+  {NULL, NULL}
 };
 
 int luaopen_codec(lua_State *L)
 {
-	luaL_register(L, "codec", codec);
-	return 1;
+  luaL_register(L, "codec", codec);
+  return 1;
 }
